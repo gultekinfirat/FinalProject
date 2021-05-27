cbeb7a76d54d6311822f214e3ae2b3de5d4e4a11
FIRAT GÜLTEKİN <20184703040@ogr.akdeniz.edu.tr>
2021-05-27 21:17:47 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product:IEntity// Public koymak; diğer katmanlar da bu sınıfa ulaşabilsin demektir.
+    {
+        public int ProductId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public short UnitsInStock { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+
+
+
+
+
+    }
+}
